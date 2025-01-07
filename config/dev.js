a954f1f3a9df61c9d3a5c79c8a9f2f9b890e30c6
DAndre06d <dumalayda06@gmail.com>
2025-01-07 23:14:31 +0800

    .

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,6 @@ module.exports = {
     googleClientID:
         "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
     googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-    mongoURI:
-        "mongodb+srv://wizdevin990:ulxFDoL3Sic8xbxd@devflow.wccri.mongodb.net/?retryWrites=true&w=majority&appName=DevFlow",
+    mongoURI: process.env.MONGODBURI,
     cookieKey: "123123123",
 };
